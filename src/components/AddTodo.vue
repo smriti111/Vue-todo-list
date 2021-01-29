6651eae4d36321683fa94c0b178ef80d85c0d733
Smriti111 <smritikpriya@gmail.com>
2021-01-29 11:07:26 +0530

    initial commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <form @submit.prevent="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="Add Todo..">
+            {{title}}
+            <input type="submit" name="title" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+//import uuid from 'uuid';
+
+export default {
+name: "AddTodo",
+
+data() {
+    return {
+      title: ''
+    }
+  },
+
+methods: {
+    addTodo(e) {
+    e.preventDefault();
+      const newTodo = {
+        //id: uuid.v4(),
+        title: this.title,
+        completed: false
+      }
+      console.log(newTodo)
+
+      this.$emit('add-todo',newTodo);
+
+      this.title= ' ';
+
+}
+}
+}
+</script>
+
+
+<style scoped>
+  form {
+    display: flex;
+  }
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt:"Vue logo" src:"./assets/logo.png">  -->
+  <div id="app">
+    <Header/>
+    <AddTodo v-on:add-Todo="addTodo"/>
+  <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import Todos from '../components/Todos';
+//import  Header from '../components/layout/Header';
+import AddTodo from '../components/AddTodo';
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    //Header,
+    Todos,
+    AddTodo
+  },
+  data(){
+    return{
+      todos: [
+        {
+          id : 1,
+          title:'Todo1',
+          completed:false
+        },
+        {
+          id:2,
+          title:'Todo2',
+          completed:true
+        },
+        {
+          id:3,
+          title:'Todo3',
+          completed:false
+        }
+
+      ]
+    }
+  },
+  methods:{
+    deleteTodo(id){
+      this.todos=this.todos.filter(todo=>todo.id!==id);
+
+    },
+    addTodo(newTodo){
+      
+      
+      this.todos= [...this.todos, newTodo];
+    }
   }
 }
 </script>
+
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
+</style>
